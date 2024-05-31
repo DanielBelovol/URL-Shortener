6316e03ab92d31864df0bede7b3cbdfe413d0785
andrieskoo <andrieskox@gmail.com>
2024-05-31 13:26:36 +0200

    Add migrations

---
+++ src/main/resources/db/migration/V1__create_tables.sql
@@ -0,0 +1,36 @@
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+CREATE TABLE roles (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE users (
+    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    name VARCHAR(255) NOT NULL,
+    lastname VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    active BOOLEAN NOT NULL DEFAULT TRUE,
+    locked BOOLEAN NOT NULL DEFAULT FALSE,
+    role_id INTEGER REFERENCES roles(id)
+);
+
+CREATE TABLE url (
+    id SERIAL PRIMARY KEY,
+    short_url VARCHAR(255) NOT NULL UNIQUE,
+    long_url TEXT NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    created_by UUID REFERENCES users(id),
+    valid_to TIMESTAMP
+);
+
+CREATE TABLE url_views (
+    id SERIAL PRIMARY KEY,
+    url_id INTEGER REFERENCES url(id),
+    ip_address VARCHAR(255) NOT NULL,
+    os_system VARCHAR(255) NOT NULL,
+    browser VARCHAR(255) NOT NULL,
+    referer TEXT
+);
