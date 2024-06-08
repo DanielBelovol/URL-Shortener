9fd8733471445a7ad694b1d13ca822f4913b3c3d
DanielBelovol <danielbelovol08@gmail.com>
2024-06-08 23:29:35 +0300

    added role enum and permission

--- src/main/java/com/example/role/Role.java
+++ src/main/java/com/example/role/Role.java
@@ -1,3 +1,43 @@
 package com.example.role;
-public class Role {
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import static com.example.role.Permission.*;
+
+@RequiredArgsConstructor
+public enum Role {
+    USER(Collections.EMPTY_SET),
+    ADMIN(Set.of(
+            ADMIN_READ,
+            ADMIN_UPDATE,
+            ADMIN_CREATE,
+            ADMIN_DELETE,
+            MODERATOR_READ,
+            MODERATOR_UPDATE,
+            MODERATOR_CREATE,
+            MODERATOR_DELETE
+    )),
+    MODERATOR(Set.of(
+            MODERATOR_READ,
+            MODERATOR_UPDATE,
+            MODERATOR_CREATE,
+            MODERATOR_DELETE
+            ));
+    @Getter
+    private final Set<Permission> permissions;
+    public List<SimpleGrantedAuthority> getAuthorities(){
+        var authorities = getPermissions()
+                .stream()
+                .map(permission -> new SimpleGrantedAuthority(permission.name()))
+                .collect(Collectors.toList());
+        authorities.add(new SimpleGrantedAuthority("ROLE_"+this.name()));
+        return authorities;
+    }
 }
--- src/main/java/com/example/user/User.java
+++ src/main/java/com/example/user/User.java
@@ -1,15 +1,20 @@
 package com.example.user;
 
-import com.example.role.RoleOld;
+import com.example.role.Role;
 import jakarta.persistence.*;
 import lombok.Data;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.time.LocalDateTime;
+import java.util.Collection;
+import java.util.List;
 
 @Data
 @Entity
 @Table(name = "users")
-public class User {
+public class User implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -35,5 +40,10 @@ public class User {
 
     @ManyToOne
     @JoinColumn(name = "role_id")
-    private RoleOld role;
+    private Role role;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return role.getAuthorities();
+    }
 }
