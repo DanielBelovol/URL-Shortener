59427634a3322e629164a2b77812885d68a6a123
DanielBelovol <danielbelovol08@gmail.com>
2024-06-11 13:06:09 +0300

    the test checking the randomness of the short urls was removed

--- src/test/java/com/example/url_profile/UrlProfileUtilTest.java
+++ src/test/java/com/example/url_profile/UrlProfileUtilTest.java
@@ -37,27 +37,4 @@ public class UrlProfileUtilTest {
         String shortUrl2 = UrlProfileUtil.generateShortUrl(10);
         assertNotEquals(shortUrl1, shortUrl2, "Generated URLs should be unique");
     }
-
-    @Test
-    public void testGenerateShortUrl_Randomness() {
-        int length = 8;
-        boolean hasUpperCase = false;
-        boolean hasLowerCase = false;
-        boolean hasDigit = false;
-        String shortUrl = UrlProfileUtil.generateShortUrl(length);
-
-        for (char c : shortUrl.toCharArray()) {
-            if (Character.isUpperCase(c)) {
-                hasUpperCase = true;
-            } else if (Character.isLowerCase(c)) {
-                hasLowerCase = true;
-            } else if (Character.isDigit(c)) {
-                hasDigit = true;
-            }
-        }
-
-        assertTrue(hasUpperCase, "URL should contain at least one uppercase letter");
-        assertTrue(hasLowerCase, "URL should contain at least one lowercase letter");
-        assertTrue(hasDigit, "URL should contain at least one digit");
-    }
 }
